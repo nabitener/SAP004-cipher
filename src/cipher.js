0ee251ebe8344ace2eccab10782d333fee7986c1
nabitener <na_bitener@hotmail.com>
2020-04-02 10:18:50 -0300

    Até aqui está funcionando o básico

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,31 @@
 const cipher = {
-  // ...
+  encode: function (string, offset) {
+
+    let valor = " ";
+    for (let i = 0; i < string.length; i++) {
+
+      let c = string.charAt(i);
+      let d = (((c.charCodeAt(0) - 65 + offset) % 26) + 65);
+      let e = String.fromCharCode(d);
+
+      valor += e;
+
+    }
+    return valor;
+  },
+  decode: function (string, offset) {
+
+    let valor = " ";
+    for (let i = 0; i < string.length; i++) {
+
+      let c = string.charAt(i);
+      let d = (((c.charCodeAt(0) - 65 - offset) % 26) + 65);
+      let e = String.fromCharCode(d);
+
+      valor += e;
+    }
+    return valor;
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+let botCifra = document.getElementById("botao-cifra");
+botCifra.addEventListener("click", cifra);
+
+let botDecifra = document.getElementById("botao-decifra");
+botDecifra.addEventListener("click", decifra);
+
+
+function cifra(codigo, offset) {
+    codigo = document.getElementById("msg").value.toUpperCase();
+    offset = parseInt(document.getElementById("chave").value);
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = cipher.encode(codigo, offset);
+    
+    return resultado;
+}
+
+function decifra(codigo, offset) {
+    codigo = document.getElementById("msg").value.toUpperCase();
+    offset = parseInt(document.getElementById("chave").value);
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = cipher.decode(codigo, offset);
+
+    return resultado;
+}
+
+
+
+
+
