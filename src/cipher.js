f1ccf626e8adca07a8d1970512df7d23f9135762
nabitener <na_bitener@hotmail.com>
2020-04-10 17:35:26 -0300

    Finalização

--- src/cipher.js
+++ src/cipher.js
@@ -4,21 +4,19 @@ const cipher = {
     let valor = "";
 
     for (let i = 0; i < string.length; i++) {
-
       let c = string.charCodeAt(i);
-      
+     
       if (c >= 65 && c <= 90) {
-
         let d = (((c - 65 + offset) % 26) + 65);
         let e = String.fromCharCode(d);
         valor += e;
-      }if (c >= 97 && c <= 122) {
+      }else if (c >= 97 && c <= 122) {
         let d = (((c - 97 + offset) % 26) + 97);
         let e = String.fromCharCode(d);
         valor += e;
-      }
-      espaco(c, valor);
-    }
+      }else if (c == 32) { 
+        valor += " ";
+      } }
     return valor;
   },
   decode: function (offset, string) {
@@ -26,18 +24,19 @@ const cipher = {
     let valor = "";
 
     for (let i = 0; i < string.length; i++) {
-
       let c = string.charCodeAt(i);
-      
+    
       if (c >= 65 && c <= 90) {
         let d = (((c - 90 - offset) % 26) + 90);
         let e = String.fromCharCode(d);
         valor += e;
-      }if (c >= 97 && c <= 122) {
+      }else if (c >= 97 && c <= 122) {
         let d = (((c - 122 - offset) % 26) + 122);
         let e = String.fromCharCode(d);
         valor += e;
-      }espaco(c, valor);  }
+      }else if (c == 32) { 
+        valor += " ";}
+      }
     return valor;
   }};
 
@@ -53,15 +52,6 @@ function validacao(offset, string) {
   if (typeof string != "string") {
     throw TypeError("Por favor digite apenas letras sem acentos")
   }
-}
-
-
-//Funções de espaço, maiusculo, minusculo
-
-function espaco(c, valor) {
-  if (c == 32)
-   { valor += " ";}
-  return valor; 
 }
 
 export default cipher;
